7a802d4929195b7c7bc93ac036df74ddb230bc2f
Tyrone Davison <t.p.davison@tees.ac.uk>
2022-11-21 21:12:25 +0000

    Add bearer auth for API calls

--- SecureWeb/Services/Values/ValuesService.cs
+++ SecureWeb/Services/Values/ValuesService.cs
@@ -1,25 +1,53 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 namespace SecureWeb.Services.Values;
 
 public class ValuesService : IValuesService
 {
-    private readonly HttpClient _client;
+    private readonly IHttpClientFactory _clientFactory;
     private readonly IConfiguration _configuration;
 
-    public ValuesService(HttpClient client,
+    public ValuesService(IHttpClientFactory clientFactory,
                          IConfiguration configuration)
     {
-        var baseAddress = configuration["Services:Values:BaseAddress"];
-        client.BaseAddress = new Uri(baseAddress);
-        _client = client;
+        _clientFactory = clientFactory;
         _configuration = configuration;
     }
 
+    record TokenDto(string access_token, string token_type, int expires_in);
+
     public async Task<ValuesGetDto> Get()
     {
-        var response = await _client.GetAsync("api/values");
+        var tokenClient = _clientFactory.CreateClient();
+
+        var authBaseAddress = _configuration["Auth:Authority"];
+        tokenClient.BaseAddress = new Uri(authBaseAddress);
+
+        var tokenParams = new Dictionary<string, string> {
+            { "grant_type", "client_credentials" },
+            { "client_id", _configuration["Auth:ClientId"] },
+            { "client_secret", _configuration["Auth:ClientSecret"] },
+            { "audience", _configuration["Services:Values:AuthAudience"] },
+        };
+        var tokenForm = new FormUrlEncodedContent(tokenParams);
+        var tokenResponse = await tokenClient.PostAsync("oauth/token", tokenForm);
+        tokenResponse.EnsureSuccessStatusCode();
+        var tokenInfo = await tokenResponse.Content.ReadFromJsonAsync<TokenDto>();
+
+        // FIXME: token should be cached rather than obtained each call
+
+
+        var client = _clientFactory.CreateClient();
+
+        var serviceBaseAddress = _configuration["Services:Values:BaseAddress"];
+        client.BaseAddress = new Uri(serviceBaseAddress);
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", tokenInfo?.access_token);
+
+        var response = await client.GetAsync("api/values");
         response.EnsureSuccessStatusCode();
+
         var result = await response.Content.ReadAsAsync<ValuesGetDto>();
         return result;
     }
